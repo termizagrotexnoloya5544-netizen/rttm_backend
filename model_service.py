fc75712ec157b7bad9b719cf741b5174c269ee92
termizagrotexnoloya5544-netizen <termizagrotexnoloya5544@gmail.com>
2025-10-30 14:39:33 +0500

    Add files via upload

---
+++ model_service.py
@@ -0,0 +1,8 @@
+def predict_image(file_path: str) -> str:
+    """Test uchun minimal funksiya.
+    Fayl nomida 'sick' bo‘lsa kasallik, aks holda sog‘lom.
+    """
+    if "sick" in file_path.lower():
+        return "Kasallik aniqlangan"
+    else:
+        return "O‘simlik sog‘lom"
